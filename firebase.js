94dd38650526ae0fd735e79691ed7e33e39f3339
YesterMester <74939964+YesterMester@users.noreply.github.com>
2025-08-26 01:59:08 -0400

    1.9.8

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
+// firebase.js
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCEMU7Sp8WXQ0fGQRfhtY_P15JDo6eI0Wk",
   authDomain: "yester-chat.firebaseapp.com",
@@ -19,3 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// --- NEW: Initialize and export Auth & Firestore ---
+export const auth = getAuth(app);
+export const db = getFirestore(app);
